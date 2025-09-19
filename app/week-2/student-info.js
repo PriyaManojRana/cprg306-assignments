6e3678c007f3f2087bf727d8eeead79c01c43692
PriyaManojRana <piya80464@gmail.com>
2025-09-19 13:26:35 -0600

    Lab 2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,17 +3,15 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <div>
-      <h2>Priya Rana</h2>
+      <h2>Priya rana</h2>
       <p>
         GitHub:{" "}
-        <Link
-          href="https://github.com/PriyaManojRana/cprg306-assignments"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="https://github.com/PriyaManojRana/cprg306-assignments"
+        target="_blank"
+        rel="noopener noreferrer">
           This is my repository.
         </Link>
       </p>
-    </div>
-  );
+    </div>
+  );
 }
