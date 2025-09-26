8a6e5d2d6623cc383d4815007806daa64975ea11
PriyaManojRana <piya80464@gmail.com>
2025-09-26 14:35:49 -0600

    week3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-white dark:bg-gray-800 rounded-md p-4 shadow">
+    <li className="bg-white rounded-md p-4 shadow">
       <div className="flex justify-between items-center">
         <div>
-          <h4 className="font-bold text-gray-800 dark:text-gray-100">{name}</h4>
-          <p className="text-gray-500">Category: {category}</p>
+          <h4 className="font-bold text-black">{name}</h4>
+          <p className="text-black">Category: {category}</p>
         </div>
         <span className="bg-blue-500 text-white px-2 py-1 rounded text-sm">
           Quantity: {quantity}
