7fed43f30777955cf0456c638680927182077ead
PriyaManojRana <piya80464@gmail.com>
2025-10-24 17:18:38 -0600

    week-7

---
+++ app/week-7/item-list.js
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import Item from "./item";
+
+export default function ItemList({ items }) {
+  const [sortBy, setSortBy] = useState("name");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
+    }
+    return 0;
+  });
+
+  return (
+    <div className="max-w-md mx-auto">
+      <div className="flex items-center justify-left gap-2 mb-6">
+        <span className="font-semibold">Sort by:</span>
+        <button
+          onClick={() => setSortBy("name")}
+          className={`px-3 py-1 rounded ${
+            sortBy === "name"
+              ? "bg-blue-500 text-grey-300"
+              : "bg-gray-200 text-black"
+          }`}
+        >
+          Name
+        </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={`px-3 py-1 rounded ${
+            sortBy === "category"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-black"
+          }`}
+        >
+          Category
+        </button>
+      </div>
+
+      <ul className="space-y-3">
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ app/week-7/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import NewItem from "./new-item";
+import ItemList from "./item-list";
+import itemsData from "./items.json";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  return (
+    <main className="bg-slate-950 p-4 min-h-screen">
+      <h1 className="text-3xl font-bold text-white mb-6 text-center">Week-7 "Shopping List"</h1>
+      
+      <div className="max-w-md mx-auto mb-8">
+        <NewItem onAddItem={handleAddItem} />
+      </div>
+      
+      <ItemList items={items} />
+    </main>
+  );
+}
